fbf7ae7c91192047c0aa5bcb4f992715107cde76
VINICIUS ATANASIO DE SOUSA ALVES <44647334848@info.local>
2026-09-08 14:55:15 -0300

    finalizado o javascript na tela login

--- login/login.js
+++ login/login.js
@@ -3,11 +3,23 @@
 //let nome = "kessia" variavel permissiva (permite a troca de valor)
 
 let CampoSenha = document.getElementById("senha"); 
-const btnSenha = document.querySelector("#mostrar-senha");
+const btnSenha = document.querySelector("#mostrar-senha"); 
 
-console.log(CampoSenha);
-console.log(btnSenha);
 
+//escutador de eventos que esta esperando um click, ou seja, quando o click acontece a ação designidada contence
 btnSenha.addEventListener("click", function() {
-    alert("Cliquei no botão");
-}); //escutador de eventos que sta esperando um click
+    
+//    if(CampoSenha.type == "password")
+//     {
+//         CampoSenha.type = "text";
+//     }
+//     else
+//     {
+//         CampoSenha.type = "password";
+//     }
+
+//campo senha esta recebndo uma pergunta, campo senha é igual a password? se sim ela recebe texto, se não ela recebe password
+CampoSenha.type = CampoSenha.type == "password" ? "text" : "password"
+
+
+}); 
